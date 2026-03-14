bcfc5df8c9998c65f6ba376376d0ff02ea5dbe0b
Mamoudou18 <mamoudou.traore180422@gmail.com>
2026-03-14 20:00:08 +0100

    feat: front > gérer dynamiquement le nom de l'utilisateur connecté + affichage decertains éléments selon le rôle

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,5 +1,12 @@
 export const localStorageKey = "currentUser";
 
+//initialisation des fonctions
+export function init(){
+    showAndHideElementsforRoles();
+    updateHeader();
+}
+ init();
+
 // Récup du bouton signout
 const signoutBtn = document.getElementById("btn-signout");
 if (signoutBtn) {
@@ -22,6 +29,13 @@ export function getToken(){
     return user ? user.api_token : null;
 }
 
+// Récuperer l'utilisateur
+export function getUser(){
+    return getStorage();
+}
+console.log("role:", getRole());
+
+
 // Fonction pour récupérer le rôle si besoin
 export function getRole(){
     const user = getStorage();
@@ -29,7 +43,7 @@ export function getRole(){
 }
 
 // Fonction pour vide le contenu du localStorage
- function removeStorage(){
+ export function removeStorage(){
     localStorage.removeItem(localStorageKey);
  }
 
@@ -37,5 +51,59 @@ export function getRole(){
 function signout(){
     removeStorage();
     window.location.reload();
+}
+
+// Vérifier si connecté
+export function isConnected(){
+    const token = getToken();
+    const user = getStorage();
+    return !!(token && user);
+}
+
+//Affichage de certains éléments selon le rôle
+export function showAndHideElementsforRoles(){
+    const ROLES = {
+        ADMIN: "admin",
+        EMPLOYE: "employe",
+        UTILISATEUR: "utilisateur"
+    }
+    const userConnected = isConnected();
+    const role = getRole();
+
+    document.querySelectorAll('[data-show]').forEach(element => {
+        switch(element.dataset.show){
+            case 'disconnected':
+                if(userConnected) element.classList.add("d-none");
+                break;
+            case 'connected':
+                if(!userConnected) element.classList.add("d-none");
+                break;
+            case 'ADMIN':
+                if(!userConnected || role !== ROLES.ADMIN) element.classList.add("d-none");
+                break;
+            case 'EMPLOYE':
+                if(!userConnected || role !== ROLES.EMPLOYE) element.classList.add("d-none");
+                break;
+            case 'UTILISATEUR':
+                if(!userConnected || role !== ROLES.UTILISATEUR) element.classList.add("d-none");
+                break;
+        }
+    });
+}
+
+// Afficher le nom de l'utilisateur connecté dans le header
+function updateHeader(){
+    const dropdown   = document.querySelector('.nav-item.dropdown');
+    const userLabel  = dropdown.querySelector('.nav-link.dropdown-toggle');
+
+    const userConnected  = isConnected();
+
+    if(userConnected){
+       const user = getStorage();
+        dropdown.style.display = 'block';
+        userLabel.innerHTML = `<span class="bi bi-person-circle"></span> ${user.prenom}`;
+    }else{
+        dropdown.style.display   = 'none';
+    }
 }
 
