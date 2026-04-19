ff199bce7e7afdbc6020bfd991042ad8142ef991
Mamoudou18 <mamoudou.traore180422@gmail.com>
2026-04-19 19:08:13 +0200

    enlever vendor de gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e96139831cd81fd1d0127284c2905b70fa7edc0d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e96139831cd81fd1d0127284c2905b70fa7edc0d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
